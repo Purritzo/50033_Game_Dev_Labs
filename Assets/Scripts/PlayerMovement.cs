495569fd94a421fe95908b6a8a9641f4140ed868
Jovi Tan <xxpurelistxx@gmail.com>
2025-01-29 00:25:41 +0800

    feat: initial left right movement inputs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 10;
+    private Rigidbody2D playerBody;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        // Set to be 30 FPS
+        Application.targetFrameRate =  30;
+        playerBody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // FixedUpdate is called 50 times a second
+    void  FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        playerBody.AddForce(movement * speed);
     }
 }
